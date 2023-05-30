091c8f199c99e6feaa4a9103294f31f55df95056
Mads942n <117907000+madsbruhn00@users.noreply.github.com>
2023-05-30 16:00:10 +0200

    Laptop commit

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <div className="App">
       
-    </div>
+      </div>
+    </>
+    
   );
 }
 
---
+++ my-app/src/Components/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <>
+    <div className='Header'>
+        <button><i className="fa-solid fa-arrow-left"></i></button>
+    </div>
+    </>
+    
+  )
+}
+
+export default Header
