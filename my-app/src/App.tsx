1f487db497b27fdb6fa56707047802aedf0a5523
Mads942n <117907000+madsbruhn00@users.noreply.github.com>
2023-05-31 23:02:12 +0200

    Finished 2 out the 5 sites, only 1 small css/element problem under datapoints.

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header';
+import { BrowserRouter as Router, Routes, Route, Link, BrowserRouter } from 'react-router-dom';
+import Home from './Pages/Home';
+import Datapoints from './Pages/Datapoints';
+import Testresults from './Pages/Testresults';
+
 
 function App() {
   return (
     <>
       <div className="App">
-      
+        <BrowserRouter>
+        <nav>
+          <Header />
+        </nav>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/Datapoints" element={<Datapoints />} />
+            <Route path='/Testresults' element={<Testresults />} />
+          </Routes>
+          </BrowserRouter>
       </div>
-    </>
+      </>
     
   );
 }
--- my-app/src/Components/Header.tsx
+++ my-app/src/Components/Header.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import styles from './Header.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft, faHouse } from '@fortawesome/free-solid-svg-icons'
+import Headerbuttons from './Headerbuttons'
+import { useNavigate } from 'react-router-dom';
 
-function Header() {
+type HeaderProps = {
+  };
+
+function Header(props: HeaderProps) {
+
+    const currentHref = window.location.href
+    const powerHref = 'http://localhost:3000/'
+
   return (
     <>
-    <div className='Header'>
-        <button><i className="fa-solid fa-arrow-left"></i></button>
+    <div className={styles.Header}>
+        <Headerbuttons icon='back' to='/' children={undefined}/>
+        <Headerbuttons icon='home' to='/' children={undefined}/>
+        {currentHref === powerHref && <Headerbuttons icon='power' to='/' children={undefined}/>}
+        {currentHref !== powerHref && <Headerbuttons icon='power' to='/' children={undefined}/>}
     </div>
     </>
     
